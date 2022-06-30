eeab23d7be0f45f9e70f29791448c1429e3929a4
hassanbaryal <h.baryal2@gmail.com>
2022-06-29 22:48:25 -0400

    Added line to set the month on the calendar

--- script.js
+++ script.js
@@ -3,16 +3,10 @@
 let today = new Date()
 
 const calendar = document.querySelector(".calendar")
+const monthElement = document.querySelector(".month")
 const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-// for (let i = 0; i <= 3; i++) {
-//     const node = document.createElement("span")
-//     node.textContent = i
-//     node.classList.add(i)
-//     calendar.appendChild(node)
-// }
 
 createCalendar()
 
@@ -21,13 +15,12 @@ function createCalendar () {
     let currentDate = today.getDate()
     let currentDay = today.getDay() - 1 //gets todays day (e.g. monday) in number form (e.g. mon = 0)
     let currentMonth = today.getMonth() //gets current month in number form (e.g. February = 1)
-
+    monthElement.textContent = months[currentMonth]
     // Calculate which day the 1st of the month falls on
     let remainder = (currentDate % 7) - 1
     let firstOfMonth = currentDay - remainder
     if (firstOfMonth < 0) firstOfMonth += 7
 
-    console.log(months[currentMonth])
     addToCalendar(firstOfMonth, '-')
     addToCalendar(daysInMonth[currentMonth], 1, currentDate)
 
